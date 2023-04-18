a4d88b706c2a352b4333b005c6efa9602ad741bd
cerium-demo <103810507+cerium-demo@users.noreply.github.com>
2023-04-18 08:55:37 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,20 @@ export default function Home() {
         </div>
       </Modal>
       <Head>
+        <script type="text/javascript" charset="utf-8">
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
+    environment: 'usw2',
+    deploymentId: 'fb055e8c-95e5-4ef8-98a4-8a1e0c74cdd8'
+  });
+        </script>
         <title>Cerium Networks</title>
 
         <meta
