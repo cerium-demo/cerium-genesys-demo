ee5cbc0dd9f2e6fe330cabec11c2c6396597f9de
cerium-demo <103810507+cerium-demo@users.noreply.github.com>
2023-04-20 13:48:21 -0700

    Update supportcenter.tsx

--- pages/supportcenter.tsx
+++ pages/supportcenter.tsx
@@ -70,15 +70,37 @@ export default function Home() {
             </nav>
           </div>
         </div>
-	</header>
-			  
-	<section className="my-28">
+        <div className="container pt-28 h-[700px] flex items-center justify-center">
+          <div className="flex flex-col items-center text-center gap-5">
+            <h1 className="text-6xl font-semibold leading-tight max-w-3xl relative">
+              <div id="genesys-support-center"></div>
+			  <div className="z-10 relative">
+                We connect your business to its potential
+              </div>
+              <div className="h-2 w-[280px] bg-primary-light transform -translate-x-4 -translate-y-5 z-0 right-0 absolute" />
+            </h1>
+            <h2 className="text-2xl text-gray-500 max-w-xl leading-relaxed">
+              Cerium provides a full suite of collaboration, networking, data
+              center, and security solutions.
+            </h2>
+            <div className="pt-5 pb-10">
+              <a href="#contact-section" className="btn btn-lg block text-lg">
+                Contact us
+              </a>
+            </div>
+            <Image src={partners} alt="Partners" />
+          </div>
+          <div className="absolute -z-10">
+            <Image src={ellipsesIllustration} alt="Illustration" />
+          </div>
+        </div>
+      </header>
+      <section className="my-28">
         <div className="container">
           <div className="text-center flex flex-col items-center gap-5">
             <div className="text-3xl font-semibold">
               We focus on your success
             </div>
-	 
             <p className="text-gray-500 max-w-xl leading-loose">
               Our consultative approach, deep technical expertise, local
               resources, and extensive strategic partnerships enables us to
